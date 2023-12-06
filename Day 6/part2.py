527b00155ec4c2a64ace91d3487330adc3f1550c
jakob22r <92209540+jakob22r@users.noreply.github.com>
2023-12-06 09:27:25 +0100

    A better solution

--- Day 6/part2.py
+++ Day 6/part2.py
@@ -1,17 +1,34 @@
 import sys
 
 #Given a time and the current best distance, what are the possible number of ways to beat this:
-def num_ways_to_win(time,dist):
-    #Count number of ways to beat currect best
+def num_ways_to_win_linear(time,dist):
     cnt = 0
-    # I is how many millisecs we hold the button for, actuall
     for i in range(time+1):
-        speed = i #pr millisecond
-        time_to_move = time-i
-        traveled = speed * time_to_move
+        traveled = i*(time-i)
         if traveled > dist:
             cnt += 1
     return cnt
+
+def num_ways_to_win_bin(time, dist):
+    # i*(time-i) will be in the middle and this is here we find our op solution
+    # Assume winner solution in the middle, i.e. i = int(t/2) exists
+    traveled = lambda i: i * (time - i)
+    #Find first occurence on left side of optimal solution which is a winn
+    l = 0
+    r = int(t/2) #This is the optimal solution which we know must exist
+    while l <= r:
+        m = int(l+(r-l)/2) #To avoid overflows
+        if traveled(m) >= dist:
+            r = m - 1
+        else:
+            l = m + 1
+    #This is the 'lowest' index for a valid win
+    lowest = l
+    #Assumming symmetry, we can figure out the highest index for a valid win
+    middle = int(time/2) #This is the index for which we are certain there is a valid win
+    last = middle + (middle-lowest)
+    return (last-lowest) + 1
+
 
 # --- PARSING ---
 input = open(sys.argv[1]).read().strip()
@@ -19,5 +36,6 @@ lines = input.split("\n")
 t  = int(''.join(filter(str.isdigit, lines[0])))
 d  = int(''.join(filter(str.isdigit, lines[1])))
 
-print(num_ways_to_win(t, d))
+print(num_ways_to_win_bin(t, d))
+print(num_ways_to_win_linear(t, d))
 
